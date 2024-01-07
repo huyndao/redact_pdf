709bb4d0e3177d503f8e526e946062b35906e01e
huyndao <40338813+huyndao@users.noreply.github.com>
2024-01-07 18:45:51 -0500

    tweaked compression level to optimize file size

--- redact_pdf.sh
+++ redact_pdf.sh
@@ -3,6 +3,6 @@
 IN=$1
 qpdf --decrypt --remove-restrictions --split-pages $IN split_%d.pdf
 for i in split_*.pdf; do
-    convert -density 600x600 $i -alpha remove -alpha off ${i%.*}.png && gimp ${i%.*}.png && convert -compress jpeg -quality 80 ${i%.*}.png ${i%.*}_redacted.pdf
+    convert -density 600x600 $i -alpha remove -alpha off ${i%.*}.png && gimp ${i%.*}.png && convert -compress jpeg -quality 75 ${i%.*}.png ${i%.*}_redacted.pdf
 done
-qpdf --empty --pages *_redacted.pdf -- $(echo ${IN#.*/} | sed 's/\.pdf/_redacted\.pdf/')
+qpdf --empty --recompress-flate --compression-level=9 --pages *_redacted.pdf -- $(echo ${IN#.*/} | sed 's/\.pdf/_redacted\.pdf/')
