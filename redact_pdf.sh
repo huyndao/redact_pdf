d0cd0273a5544bed4ef384ab8b54211c599fc403
huyndao <40338813+huyndao@users.noreply.github.com>
2024-01-07 18:52:08 -0500

    more optimization

--- redact_pdf.sh
+++ redact_pdf.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 IN=$1
-qpdf --decrypt --remove-restrictions --split-pages $IN split_%d.pdf
+qpdf --decrypt --remove-restrictions --optimize-images --recompress-flate --compression-level=9 --split-pages $IN split_%d.pdf
 for i in split_*.pdf; do
     convert -density 600x600 $i -alpha remove -alpha off ${i%.*}.png && gimp ${i%.*}.png && convert -compress jpeg -quality 75 ${i%.*}.png ${i%.*}_redacted.pdf
 done
-qpdf --empty --recompress-flate --compression-level=9 --pages *_redacted.pdf -- $(echo ${IN#.*/} | sed 's/\.pdf/_redacted\.pdf/')
+qpdf --empty --optimize-images --recompress-flate --compression-level=9 --pages *_redacted.pdf -- $(echo ${IN#.*/} | sed 's/\.pdf/_redacted\.pdf/')
